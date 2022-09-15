ea67acdf0c7b5b66eb836f4e3c646fff8e82fcd8
abhishek <trabhishek17@gmail.com>
2022-09-15 18:04:01 +0530

    commited

---
+++ LibraryAPI/Services/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using LibraryAPI.Models;
+using System;
+
+
+namespace LibraryAPI.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+        Book Add(Book newBook);
+        Book GetById(Guid id);
+        Book Remove(Guid id);
+    }
+}
